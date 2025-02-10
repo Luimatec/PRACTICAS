5fee5784ae9fdac26fa4a8acc9d8cdae2e0b6601
luism <lmtejada37@gmail.com>
2025-02-10 19:58:04 -0400

    Commit practica

---
+++ Crear Clases.js
@@ -0,0 +1,19 @@
+[
+    {"Nombre": "Luima",
+   "Edad": "28",
+   "Especialidad": "Biologia",
+   "Grado": 3
+    },
+   
+    {"Nombre": "Pedro Maria",
+   "Edad": "32",
+   "Especialidad": "Fisica",
+   "Grado": 3
+    },
+   
+    {"Nombre": "Jenifer",
+   "Edad": "25",
+    "Especialidad":"Quimica",
+   "Grado": 4
+    }
+   ]
